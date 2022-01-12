76e8813ea921401927cce071ac77c7456dcb7309
Gregory Brooks <gregorybrooks@comcast.net>
2022-01-12 12:10:06 -0500

    add do_all.sh

--- src/main/sql/group_them_ordered_gap.sql
+++ src/main/sql/group_them_ordered_gap.sql
@@ -1,6 +1,8 @@
-truncate table counts_ordered_gap;
+drop table if exists counts_ordered_gap;
+create table counts_ordered_gap (first_term varchar(100), second_term varchar(100), index int);
 copy counts_ordered_gap from '/data/collectione-tokenized.tsv.counts_ordered_gap.counts' with (format csv);
-truncate table counts_ordered_gap_grouped;
+drop table if exists counts_ordered_gap_grouped;
+create table counts_ordered_gap_grouped (first_term varchar(100), second_term varchar(100), index int, count int);
 insert into counts_ordered_gap_grouped select first_term, second_term, index, count(*) as count from counts_ordered_gap group by first_term, second_term, index;
 create index counts_ordered_gap_grouped_x1 on counts_ordered_gap_grouped(first_term);
 copy counts_ordered_gap_grouped to '/data/collectione-tokenized.tsv.counts_ordered_gap.grouped.csv' with (format csv);
